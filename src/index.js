b21f3dd76b8e47d94c869211c3d63624e6e99492
albert <albertincx@gmail.com>
2020-06-13 09:19:03 +0300

    c

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const botroute = require('./api/routes/botroute');
 const api = require('./api/routes/api');
 const conn = mongoose.connect();
 
-<<<<<<< HEAD
 if(!process.env.FILESLAVE) {
   app.get('/', (req, res) => res.send('use telegram bot <a href="tg://resolve?domain=InstantChatBot">@InstantChatBot</a>'));
   app.use(api);
@@ -18,27 +17,9 @@ if(!process.env.FILESLAVE) {
     if (botInstance) {
       const { router, bot } = botroute(botInstance, conn);
       bot.setBlacklist(blacklistFile);
-      if(process.env.FILESLAVE) {
-        require('./ws.js')(bot)
-      }
+      require('./ws.js')(bot)
     }
   }
 }
 
-
-
-=======
-app.get('/', (req, res) => res.send('use telegram bot <a href="tg://resolve?domain=CorsaBot">@CorsaBot</a>'));
-app.use(api);
-let botInstance;
-if (!NOBOT && process.env.TBTKN) {
-  botInstance = require('./config/bot');
-  if (botInstance) {
-    const { router, bot } = botroute(botInstance, conn);
-    bot.setBlacklist(blacklistFile);
-    app.use('/bot', router);
-  }
-}
-app.listen(PORT, () => console.info(`server started on port ${PORT}`));
->>>>>>> 44d5633fd3a2eac5042fed894b309cbf9633daf7
 module.exports = app;
