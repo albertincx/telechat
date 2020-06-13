4baa3ed2b84919fa62c64a08869f61e149f9b557
Glitch (albertincx-telechat-2) <none>
2020-06-13 08:02:09 +0000

    😋🍿 Checkpoint ./copy.sh:6304743/51 ./3.0c2321c409c654b320d4.chunk.js:6304743/136267 ./.glitch-assets:6304743/1376 ./index.html:6304743/419

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
 find ./ -maxdepth 1 -type f -name '*.js' -exec mv {} ./public/apps/ \;
 find ./ -maxdepth 1 -type f -name '*.html' -exec mv {} ./public/apps/ \;
 find ./ -maxdepth 1 -type f -name '*.txt' -exec mv {} ./public/apps/ \;
+#mv ./public/apps/ii.html ./public/apps/index.html
