2c3a10722fffc2c3c9f78da23b04d944454ac37f
Glitch (albertincx-telechat-2) <none>
2020-06-13 08:25:16 +0000

    🤗🚲 Checkpoint ./.glitch-assets:6304743/1312 ./build.txt:6304743/785328 ./copy.sh:6304743/249

--- copy.sh
+++ copy.sh
@@ -1,4 +1,6 @@
-find ./ -maxdepth 1 -type f -name '*.js' -exec mv {} ./public/apps/ \;
-find ./ -maxdepth 1 -type f -name '*.html' -exec mv {} ./public/apps/ \;
-find ./ -maxdepth 1 -type f -name '*.txt' -exec mv {} ./public/apps/ \;
+#find ./ -maxdepth 1 -type f -name '*.js' -exec mv {} ./public/apps/ \;
+#find ./ -maxdepth 1 -type f -name '*.html' -exec mv {} ./public/apps/ \;
+#find ./ -maxdepth 1 -type f -name '*.txt' -exec mv {} ./public/apps/ \;
 #mv ./public/apps/ii.html ./public/apps/index.html
+cp ./build.txt ./build.tar.gz
+tar -C ./public/apps -xzvf ./build.tar.gz
