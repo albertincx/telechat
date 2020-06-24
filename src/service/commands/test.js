8d3cb627397afa97c91dbccf5e8064b2dfbf7a88
albert <albertincx@gmail.com>
2020-06-24 17:03:06 +0300

    c

---
+++ src/service/commands/test.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+async function run(params, botHelper) {
+  try {
+    let url = process.env.TEST_API;
+    if (!url) return;
+    await botHelper.clearUnusedChats();
+    // await botHelper.sendAdmin('cron test check url', process.env.TGGROUP);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = { run };
