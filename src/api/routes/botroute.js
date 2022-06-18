07c3ca3c14728a40934b33da321b97dc5578bb55
albertincx <albertincx@gmail.com>
2022-06-18 12:06:03 +0200

    data type

--- src/api/routes/botroute.js
+++ src/api/routes/botroute.js
@@ -6,7 +6,7 @@ const db = require('../utils/db');
 
 const router = express.Router();
 const filepath = 'count.txt';
-if (!fs.existsSync(filepath)) fs.writeFileSync(filepath, 0);
+if (!fs.existsSync(filepath)) fs.writeFileSync(filepath, '0');
 
 let startCnt = parseInt(fs.readFileSync('count.txt'), 10);
 module.exports = (bot, conn) => {
@@ -64,6 +64,6 @@ module.exports = (bot, conn) => {
   }
   startCnt += 1;
   if (startCnt >= 500) startCnt = 0;
-  fs.writeFileSync(filepath, startCnt);
+  fs.writeFileSync(filepath, `${startCnt}`);
   return { router, bot: botHelper };
 };
--- src/api/utils/logger.js
+++ src/api/utils/logger.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const logger = (content, file) => {
   if (process.env.DEV) {
     if (file) {
-      fs.writeFileSync(`.conf/${file}`, content);
+      fs.writeFileSync(`.conf/${file}`, `${content}`);
     } else {
       console.log(content);
     }
